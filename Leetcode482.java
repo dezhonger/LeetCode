dc2ff312abb07b6d5e7bb599cceb4f754c7dc112
dezhonger <373440955@qq.com>
2017-02-19 22:55:55 +0800

    add

---
+++ Leetcode482.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+/**
+ * Created by Dezhonger on 2017/2/19.
+ * 题目很简单，看到了一个更简单的写法。学习一下。
+ */
+public class Leetcode482 {
+    public String licenseKeyFormatting(String s, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--)
+            if (s.charAt(i) != '-')
+                sb.append(sb.length() % (k + 1) == k ? '-' : "").append(s.charAt(i));
+        return sb.reverse().toString().toUpperCase();
+    }
+}
