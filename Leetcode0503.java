e4f22ccad93d76789263b89b9335255f5f1d8cdb
Dezhonger <373440955@qq.com>
2019-09-21 18:39:57 +0800

    Update and rename Leetcode503.java to Leetcode0503.java

--- Leetcode0503.java
+++ Leetcode0503.java
@@ -3,7 +3,7 @@ package p1107.p20170214;
 /**
  * Created by dezhonger on 2017/2/14.
  */
-public class Leetcode503 {
+public class Leetcode0503 {
 	public String convertToBase7(int num) {
 		return Integer.toString(num, 7);
 	}
