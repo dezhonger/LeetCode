e6d29be2031626dbcf80a76c74eb41729d820991
Dezhonger <373440955@qq.com>
2019-09-22 23:01:01 +0800

    Update and rename Leetcode392.java to Leetcode0392.java

--- Leetcode0392.java
+++ Leetcode0392.java
@@ -4,7 +4,7 @@ package p1107.p20170210;
  * Created by dezhonger on 2017/2/10.
  * 判断s是不是t的子序列，不必连续。
  */
-public class Leetcode392 {
+public class Leetcode0392 {
 	public boolean isSubsequence(String s, String t) {
 		if(s.length() == 0) return true;
 		if(t.length() == 0) return false;
