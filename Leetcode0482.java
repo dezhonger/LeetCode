89e4d774ad2ab1922ef799e51ec9eadfbcaab513
Dezhonger <373440955@qq.com>
2019-09-28 23:03:55 +0800

    Update and rename Leetcode482.java to Leetcode0482.java

--- Leetcode0482.java
+++ Leetcode0482.java
@@ -1,10 +1,9 @@
-package leetcode;
 
 /**
  * Created by Dezhonger on 2017/2/19.
  * 题目很简单，看到了一个更简单的写法。学习一下。
  */
-public class Leetcode482 {
+public class Leetcode0482 {
     public String licenseKeyFormatting(String s, int k) {
         StringBuilder sb = new StringBuilder();
         for (int i = s.length() - 1; i >= 0; i--)
