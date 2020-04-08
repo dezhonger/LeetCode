27e9865c19b939613143507b575c79670165d8c6
Dezhonger <373440955@qq.com>
2020-04-08 13:48:06 +0800

    heap

---
+++ Leetcode0857.java
@@ -0,0 +1,49 @@
+package com.netease.music.leetcode;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ * Created by dezhonger on 2020/4/8
+ */
+public class Leetcode0857 {
+
+    public double mincostToHireWorkers(int[] quality, int[] wage, int K) {
+        int N = quality.length;
+        Worker[] workers = new Worker[N];
+        for (int i = 0; i < N; ++i)
+            workers[i] = new Worker(quality[i], wage[i]);
+        Arrays.sort(workers);
+
+        double ans = 1e9;
+        int sumq = 0;
+        PriorityQueue<Integer> pool = new PriorityQueue();
+        for (Worker worker: workers) {
+            pool.offer(-worker.quality);
+            sumq += worker.quality;
+            if (pool.size() > K)
+                sumq += pool.poll();
+            if (pool.size() == K)
+                ans = Math.min(ans, sumq * worker.ratio());
+        }
+
+        return ans;
+    }
+    
+}
+
+class Worker implements Comparable<Worker> {
+    public int quality, wage;
+    public Worker(int q, int w) {
+        quality = q;
+        wage = w;
+    }
+
+    public double ratio() {
+        return (double) wage / quality;
+    }
+
+    public int compareTo(Worker other) {
+        return Double.compare(ratio(), other.ratio());
+    }
+}
