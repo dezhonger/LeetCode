023b772985144b0dcb3c7182e923d841810b4ed2
Dezhonger <373440955@qq.com>
2020-08-19 13:17:07 +0800

    Floyd

---
+++ Leetcode1462.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Filename: Leetcode1462.cpp
+//
+//Author: dezhonger - csdisassembly@gmail.com
+
+//Create: 2020-08-19 13:08:19
+
+#define reps(i, a, b) for(int i = (a); i < (b); i++)
+#define reps(i, a, b) for(int i = (a); i < (b); i++)
+#define pb push_back
+#define ps push
+#define mp make_pair
+#define CLR(x,t) memset(x,t,sizeof x)
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+
+#define Debug(x) cout<<#x<<"="<<x<<endl;
+const double euler_r = 0.57721566490153286060651209;
+const double pi = 3.141592653589793238462643383279;
+const double E = 2.7182818284590452353602874713526;
+const int inf = ~0U >> 1;
+const int mod = int (1e9) + 7;
+const double EPS = 1e-6;
+
+typedef long long LL;
+LL powmod(LL a,LL b) {LL res=1;a%=mod; assert(b>=0); for(;b;b>>=1){if(b&1)res=res*a%mod;a=a*a%mod;}return res;}
+LL gcd(LL a,LL b) { return b?gcd(b,a%b):a;}
+
+class Solution {
+	bool d[105][105];
+	vector<bool> ans;
+public:
+    vector<bool> checkIfPrerequisite(int n, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
+		memset(d, 0, sizeof d);
+		for (int i = 0; i < n; i++) d[i][i] = 1;
+		for (auto e : prerequisites) d[e[0]][e[1]] = 1;
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					d[i][j] |= d[i][k] && d[k][j];
+				}
+			}
+		}
+		for (auto q : queries) ans.push_back(d[q[0]][q[1]]);
+		return ans;
+    }
+};
+int main() {
+    ios::sync_with_stdio (false);
+    cin.tie (0);
+    return 0;
+}
+
