e4c8e50ac34ced47239873aabf5cb3adfd1d319c
Dezhonger <373440955@qq.com>
2021-03-23 13:03:33 +0800

    Update Leetcode0745.cpp

--- Leetcode0745.cpp
+++ Leetcode0745.cpp
@@ -11,48 +11,48 @@ int son[N][27], w[N], idx;
 
 
 class WordFilter {
-public:
-	
-	void insert(string& s, int id) {
-		int root = 0;
-		for (auto c : s) {
-			int t = c == '#' ? 26 : c - 'a';
-			if (!son[root][t]) son[root][t] = ++idx;
-			root = son[root][t];
-			w[root] = id;
-		}
-	}
+	public:
 
-	int query(string s) {
-		int root = 0;
-		for (auto c : s) {
-			int t = c == '#' ? 26 : c - 'a';
-			if (!son[root][t]) return -1;
-			root = son[root][t];
-		}
-		return w[root];
-	}
-
-    WordFilter(vector<string>& words) {
-        memset(son, 0, sizeof son);
-		idx = 0;
-		for (int i = 0; i < words.size(); i++) {
-			string s = '#' + words[i];
-			for (int j = words[i].size() - 1; j >= 0; j--) {
-				s = words[i][j] + s;
-				insert(s, i);
+		void insert(string& s, int id) {
+			int root = 0;
+			for (auto c : s) {
+				int t = c == '#' ? 26 : c - 'a';
+				if (!son[root][t]) son[root][t] = ++idx;
+				root = son[root][t];
+				w[root] = id;
 			}
 		}
-    }
-    
-    int f(string prefix, string suffix) {
-        return query(suffix + '#' + prefix);
-    }
+
+		int query(string s) {
+			int root = 0;
+			for (auto c : s) {
+				int t = c == '#' ? 26 : c - 'a';
+				if (!son[root][t]) return -1;
+				root = son[root][t];
+			}
+			return w[root];
+		}
+
+		WordFilter(vector<string>& words) {
+			memset(son, 0, sizeof son);
+			idx = 0;
+			for (int i = 0; i < words.size(); i++) {
+				string s = '#' + words[i];
+				for (int j = words[i].size() - 1; j >= 0; j--) {
+					s = words[i][j] + s;
+					insert(s, i);
+				}
+			}
+		}
+
+		int f(string prefix, string suffix) {
+			return query(suffix + '#' + prefix);
+		}
 };
 
 int main() {
-    ios::sync_with_stdio (false);
-    cin.tie (0);
-    return 0;
+	ios::sync_with_stdio (false);
+	cin.tie (0);
+	return 0;
 }
 
