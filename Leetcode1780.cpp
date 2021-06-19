12cd9f52cfbf69242c238ae800f8a8faa85c24b4
Dezhonger <373440955@qq.com>
2021-06-20 03:05:09 +0800

    Create Leetcode1780.cpp

---
+++ Leetcode1780.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool checkPowersOfThree(int n) {
+        while (n) {
+            if (n % 3 == 2) return 0;
+            n /= 3;
+        }
+        return 1;
+    }
+};
