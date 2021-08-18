334a48d3919232054318a705842acb338dcc4bdf
Dezhonger <373440955@qq.com>
2021-08-18 16:11:07 +0800

    Create Leetcode1968.cpp

---
+++ Leetcode1968.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<int> res(n);
+        for (int i = 0, j = 0; i < n; i += 2) res[i] = nums[j++];
+        for (int i = 1, j = n - 1; i < n; i += 2) res[i] = nums[j--];
+        return res;
+    }
+};
