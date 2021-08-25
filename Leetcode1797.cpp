eaaace48b8a75ed79d7bbaae5e42f1b382d2b9f7
Dezhonger <373440955@qq.com>
2021-08-25 15:17:37 +0800

    treap

---
+++ Leetcode1797.cpp
@@ -0,0 +1,139 @@
+const int N = 2020, INF = 1e8;
+
+int n;
+struct Node
+{
+    int l, r;
+    int key, val;
+    int cnt, size;
+}tr[N];
+
+int root, idx;
+
+void pushup(int p)
+{
+    tr[p].size = tr[tr[p].l].size + tr[tr[p].r].size + tr[p].cnt;
+}
+
+int get_node(int key)
+{
+    tr[ ++ idx].key = key;
+    tr[idx].l = tr[idx].r = 0;
+    tr[idx].val = rand();
+    tr[idx].cnt = tr[idx].size = 1;
+    return idx;
+}
+
+void zig(int &p)    // 右旋
+{
+    int q = tr[p].l;
+    tr[p].l = tr[q].r, tr[q].r = p, p = q;
+    pushup(tr[p].r), pushup(p);
+}
+
+void zag(int &p)    // 左旋
+{
+    int q = tr[p].r;
+    tr[p].r = tr[q].l, tr[q].l = p, p = q;
+    pushup(tr[p].l), pushup(p);
+}
+
+void build()
+{
+    get_node(-INF), get_node(INF);
+    root = 1, tr[1].r = 2;
+    pushup(root);
+
+    if (tr[1].val < tr[2].val) zag(root);
+}
+
+
+void insert(int &p, int key)
+{
+    if (!p) p = get_node(key);
+    else if (tr[p].key == key) tr[p].cnt ++ ;
+    else if (tr[p].key > key)
+    {
+        insert(tr[p].l, key);
+        if (tr[tr[p].l].val > tr[p].val) zig(p);
+    }
+    else
+    {
+        insert(tr[p].r, key);
+        if (tr[tr[p].r].val > tr[p].val) zag(p);
+    }
+    pushup(p);
+}
+
+void remove(int &p, int key)
+{
+    if (!p) return;
+    if (tr[p].key == key)
+    {
+        if (tr[p].cnt > 1) tr[p].cnt -- ;
+        else if (tr[p].l || tr[p].r)
+        {
+            if (!tr[p].r || tr[tr[p].l].val > tr[tr[p].r].val)
+            {
+                zig(p);
+                remove(tr[p].r, key);
+            }
+            else
+            {
+                zag(p);
+                remove(tr[p].l, key);
+            }
+        }
+        else p = 0;
+    }
+    else if (tr[p].key > key) remove(tr[p].l, key);
+    else remove(tr[p].r, key);
+
+    pushup(p);
+}
+
+int get_rank_by_key(int p, int key)    // 通过数值找排名
+{
+    cout << p << endl;
+    if (p <= 0) return 0; 
+    if (tr[p].key == key) return tr[tr[p].l].size + 1;
+    if (tr[p].key > key) return get_rank_by_key(tr[p].l, key);
+    return tr[tr[p].l].size + tr[p].cnt + get_rank_by_key(tr[p].r, key);
+}
+
+
+
+
+class AuthenticationManager {
+public:
+    int t, sz = 0;
+    unordered_map<string, int> mp;
+    AuthenticationManager(int timeToLive) {
+        root = idx = 0;
+        build();
+        this -> t = timeToLive;
+        mp.clear();
+    }
+    
+    void generate(string tokenId, int currentTime) {
+        sz++;
+        mp[tokenId] = currentTime;
+        insert(root, currentTime);
+    }
+    
+    void renew(string tokenId, int currentTime) {
+        if (mp.find(tokenId) == mp.end()) return ;
+        int curT = mp[tokenId];
+        // 验证码过期了
+        if (curT + t <= currentTime) return ;
+        mp[tokenId] = currentTime;
+        
+        remove(root, curT);
+        insert(root, currentTime);
+    }
+    
+    int countUnexpiredTokens(int currentTime) {
+        // get_rank_by_key : 求 <= x的个数, 加
+        return sz - (get_rank_by_key(root, currentTime - t) - 1);
+    }
+};
