39b4abf5495069672b801e97b4bdd5494be67f3d
Dezhonger <373440955@qq.com>
2022-03-24 12:00:29 +0800

    Update Leetcode2076.cpp

--- Leetcode2076.cpp
+++ Leetcode2076.cpp
@@ -8,13 +8,13 @@ public:
     vector<bool> friendRequests(int n, vector<vector<int>>& restrictions, vector<vector<int>>& requests) {
         vector<bool> ans;
         for (int i = 0; i < n; i++) f[i] = i;
-        for (auto r: requests) {
+        for (auto& r: requests) {
             int x = r[0], y = r[1];
             int fx = find(x), fy = find(y);
             if (fx == fy) ans.push_back(true);
             else {
                 bool flag = true;
-                for (auto re: restrictions) {
+                for (auto& re: restrictions) {
                     int u = find(re[0]), v = find(re[1]);
                     if ((u == fx && v == fy) || (u == fy && v == fx)) {
                         flag = false;
