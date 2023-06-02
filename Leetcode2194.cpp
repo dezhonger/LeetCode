edb5d9f7353eb2a665df3d2fda8b703b10abe612
Dezhonger <373440955@qq.com>
2023-06-02 23:19:37 +0800

    Create Leetcode2194.cpp

---
+++ Leetcode2194.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> cellsInRange(string s) {
+        vector<string> res;
+        for (char c = s[0]; c <= s[3]; c++)
+        {
+            for (char r = s[1]; r <= s[4]; r++)
+            {
+                res.push_back({c, r});
+            }
+        }
+
+        return res;
+    }
+};
