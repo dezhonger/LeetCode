70ef1f9438ee83e66ed807f00e571e82bf20e24c
Dezhonger <373440955@qq.com>
2023-06-29 15:42:57 +0800

    Create Leetcode2145.cpp

---
+++ Leetcode2145.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfArrays(vector<int>& differences, int lower, int upper) {
+        int x = 0, y = 0, cur = 0;
+        for (int d: differences) {
+            cur += d;
+            x = min(x, cur);
+            y = max(y, cur);
+            if (y - x > upper - lower) {
+                return 0;
+            }
+        }
+        // cout << x << y << endl;
+        return (upper - lower) - (y - x) + 1;
+    }
+};
