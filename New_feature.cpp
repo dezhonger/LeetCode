d34bda33c0e290d526e2014ef8c32fef57289a69
Dezhonger <373440955@qq.com>
2023-07-05 11:20:50 +0800

    Update New_feature.cpp

--- New_feature.cpp
+++ New_feature.cpp
@@ -1,17 +1,27 @@
 use init function:
 
 lc2320.cpp
-const int MOD = 1e9 + 7, MX = 1e4 + 1;
-int f[MX] = {1, 2};
-int init = []() {
-    for (int i = 2; i < MX; ++i)
-        f[i] = (f[i - 1] + f[i - 2]) % MOD;
-    return 0;
-}();
+    const int MOD = 1e9 + 7, MX = 1e4 + 1;
+    int f[MX] = {1, 2};
+    int init = []() {
+        for (int i = 2; i < MX; ++i)
+            f[i] = (f[i - 1] + f[i - 2]) % MOD;
+        return 0;
+    }();
+    
+    class Solution {
+    public:
+        int countHousePlacements(int n) {
+            return (long) f[n] * f[n] % MOD;
+        }
+    };
 
-class Solution {
-public:
-    int countHousePlacements(int n) {
-        return (long) f[n] * f[n] % MOD;
-    }
-};
+
+
+function:
+
+    function<void(int)> dfs = [&](int x) {
+        vis[x] = true;
+        ++cnt;
+        for (int y: g[x]) if (!vis[y]) dfs(y);
+    };
