3a703cbfb570e13a38f3de9829938473086959d5
Dezhonger <373440955@qq.com>
2023-07-05 10:57:11 +0800

    Create New_feature.cpp

---
+++ New_feature.cpp
@@ -0,0 +1,17 @@
+use init function:
+
+lc2320.cpp
+const int MOD = 1e9 + 7, MX = 1e4 + 1;
+int f[MX] = {1, 2};
+int init = []() {
+    for (int i = 2; i < MX; ++i)
+        f[i] = (f[i - 1] + f[i - 2]) % MOD;
+    return 0;
+}();
+
+class Solution {
+public:
+    int countHousePlacements(int n) {
+        return (long) f[n] * f[n] % MOD;
+    }
+};
