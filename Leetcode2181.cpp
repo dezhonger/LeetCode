4d74e68ffd0279dea5708c4988c46a5361326942
Dezhonger <373440955@qq.com>
2023-07-11 16:04:11 +0800

    Create Leetcode2181.cpp

---
+++ Leetcode2181.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeNodes(ListNode* head) {
+        ListNode* dummy = new ListNode();
+        ListNode* tail = dummy;
+        int t = 0;
+        for (ListNode* cur = head -> next; cur; cur = cur -> next)
+        {
+            if (cur -> val == 0)
+            {
+                ListNode* node = new ListNode(t);
+                tail -> next = node;
+                tail = tail -> next;
+                t = 0;
+            }
+            else t += cur -> val;
+        }
+        return dummy -> next;
+    }
+};
